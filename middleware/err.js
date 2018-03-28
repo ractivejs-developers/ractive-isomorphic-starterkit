206711279a2775c2a47925213cc4508c3f69a965
Pavel Malyshev <pavel@mustlab.ru>
2018-03-29 00:27:50 +0300

    Method snapshot() added. Default template resolver by component name.

--- middleware/err.js
+++ middleware/err.js
@@ -1,6 +1,6 @@
 module.exports = () => (err, req, res, next) => {
 	res.status(500);
 	(req.accepts(['html', 'json']) === 'json') ?
-		res.json({ errors: { [err.name]: [err.message] } }) : 
+		res.json(err) : 
 		res.render('error', { err });
 };
--- src/app.js
+++ src/app.js
@@ -3,14 +3,22 @@ const Ractive = require('ractive');
 Ractive.DEBUG = (process.env.NODE_ENV === 'development');
 Ractive.DEBUG_PROMISES = Ractive.DEBUG;
 
-Ractive.defaults.enhance = true;
-Ractive.defaults.lazy = true;
-Ractive.defaults.sanitize = true;
-
 Ractive.use(require('ractive-ready')());
 Ractive.use(require('ractive-page')({
     meta: require('../config/meta.json')
 }));
+
+Ractive.defaults.enhance = true;
+Ractive.defaults.lazy = true;
+Ractive.defaults.sanitize = true;
+Ractive.defaults.template = function() {
+    const name = this.component && this.component.name;
+    try { return require(`./templates/parsed/${name}`) }
+    catch(e) { return 'Template unspecified' }
+};
+Ractive.defaults.snapshot = function(key = '') {
+    return `@global.__DATA__${this.keychain()}.${key}`;
+};
 
 const options = {
     el: '#app',
